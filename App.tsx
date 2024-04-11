7d99c84dae64967d00f8496c801b56a7560643ec
jayumap <umapjay4757@gmail.com>
2024-04-11 21:41:43 +0530

    static reverse geocoding done

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {
   StyleSheet,
   View,
@@ -13,6 +13,8 @@ import {PointAnnotation} from '@rnmapbox/maps';
 import Logger from '@rnmapbox/maps';
 import Mapbox from '@rnmapbox/maps';
 import Fontisto from 'react-native-vector-icons/Fontisto';
+// import { parse } from 'xml-js';
+
 
 // Logger.setLogCallback(log => {
 //   const {message} = log;
@@ -35,6 +37,10 @@ Mapbox.setTelemetryEnabled(false);
 
 const App = () => {
   const [modalVisible, setModalVisible] = useState<boolean>(false);
+  const [LocationData, setLocationData] = useState<any>(null);
+
+  // reverse geocode api
+  const url = `https://nominatim.openstreetmap.org/reverse?format=json&lat=${18.516726}&lon=${73.856255}`;
 
   const onMarkerPress = () => {
     setModalVisible(true);
@@ -43,6 +49,25 @@ const App = () => {
   const closeModal = () => {
     setModalVisible(false);
   };
+
+  useEffect(() => {
+    fetch(url)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(jsonData => {
+        setLocationData(jsonData);
+      })
+      .catch(error =>
+        console.error('Error while fetching the reverse data', error),
+      );
+  }, []);
+  
+  
+
   return (
     <View style={styles.container}>
       <Mapbox.MapView
@@ -69,11 +94,15 @@ const App = () => {
       <Modal visible={modalVisible} animationType="none" transparent>
         <View style={styles.modalContainer}>
           <View style={styles.modalContent}>
-              <TouchableOpacity onPress={closeModal} style={styles.closeButton}>
-                <Text style={styles.closeButtonText}>Close</Text>
-              </TouchableOpacity>
-              <Text>City: Pune</Text>
-              <Text>Country: India</Text>
+            <TouchableOpacity onPress={closeModal} style={styles.closeButton}>
+              <Text style={styles.closeButtonText}>Close</Text>
+            </TouchableOpacity>
+            <Text style={styles.modalText}>Road: {LocationData?.address?.road}</Text>
+            <Text style={styles.modalText}>Suburb: {LocationData?.address?.suburb}</Text>
+            <Text style={styles.modalText}>City: {LocationData?.address?.city}</Text>
+            <Text style={styles.modalText}>State District: {LocationData?.address?.state_district}</Text>
+            <Text style={styles.modalText}>State: {LocationData?.address?.state}</Text>
+            <Text style={styles.modalText}>Country: {LocationData?.address?.country}</Text>
           </View>
         </View>
       </Modal>
@@ -108,17 +137,26 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
     borderRadius: 16,
     marginHorizontal: 20,
+    marginVertical: 100,
     padding: 20,
     elevation: 5,
     width: Dimensions.get('window').width - 40,
   },
+  modalText: {
+    color: 'black', 
+    fontSize: 16,
+    marginBottom: 10,
+  },
   closeButton: {
     position: 'absolute',
-    top: 40,
+    backgroundColor: 'red',
+    borderRadius: 5,
+    top: 160,
+    bottom: 14,
     right: 20,
   },
   closeButtonText: {
-    color: 'black',
+    color: 'white',
     fontSize: 16,
     padding: 10,
   },
